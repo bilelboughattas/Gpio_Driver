6006bcf85326d99b4baa2683c00f5ff82914cffa
bilelboughattas <boughattasbilel20@gmail.com>
2022-11-19 16:49:53 +0100

    adding assertion and some improuvments

--- gpio.h
+++ gpio.h
@@ -3,12 +3,13 @@
 #ifndef __GPIO_H
 #define __GPIO_H
 #include <stdint.h>
-#include <assert.h>
 
 #define GPIOA  (uint32_t *)0x40020000
 #define GPIOB  (uint32_t *)0x40020400
 #define GPIOC  (uint32_t *)0x40020800
 #define GPIOD  (uint32_t *)0x40020c00
+#define GPIOT  (uint32_t *)0x40020c01
+
 #define PIN0 0
 #define PIN1 1
 #define PIN2 2
@@ -26,16 +27,27 @@
 #define PIN14 14
 #define PIN15 15
 
-#define INPUT           (uint8_t)0x0
-#define OUTPUT          (uint8_t)0x1
-#define ALTERNATE       (uint8_t)0x2
-#define ANALOG_INPUT    (uint8_t)0x3
+#define INPUT           0x0u
+#define OUTPUT          0x1u
+#define ALTERNATE       0x2u
+#define ANALOG_INPUT    0x3u
 
-#define OUTPUT_PUSH_PULL   (uint8_t)0x00
-#define OUTPUT_OPEN_DRAIN  (uint8_t)0x01
+#define OUTPUT_PUSH_PULL   0x0u
+#define OUTPUT_OPEN_DRAIN  0x1u
 
+#define LOW_SPEED       0X0u
+#define MEDIUM_SPEED    0x1u
+#define HIGH_SPEED      0x2u
+#define VERY_HIGH_SPEED 0x3u
+
+#define SET             1u
+#define RESET           0u
 
 void GPIO_Clock_Enable(uint32_t *GPIOx);
-void GPIO_Config(uint32_t *GPIOx, uint8_t Pin, uint8_t Mode, uint8_t Type);
+void GPIO_Config(uint32_t *GPIOx, uint8_t Pin, uint8_t Mode, uint8_t Type, uint8_t Speed);
+void GPIO_Write_Port(uint32_t *GPIOx, uint16_t PortValue);
+void GPIO_Write_Pin(uint32_t *GPIOx, uint8_t Pin, uint8_t PinValue);
+uint16_t GPIO_Read_Port(uint32_t *GPIOx);
+uint8_t GPIO_Read_Pin(uint32_t *GPIOx, uint8_t Pin);
 
 #endif
